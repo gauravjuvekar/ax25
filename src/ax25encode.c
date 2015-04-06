a3445daf3e0860369e56a1852748cacf895b4bb9
Gaurav Juvekar <juvekargs14.it@coep.ac.in>
2015-04-06 09:17:20 +0530

    Create / read given files

--- src/ax25encode.c
+++ src/ax25encode.c
@@ -1,19 +1,20 @@
 #include<argp.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<sysexits.h>
 #include<stdbool.h>
 #include "ax25encode.h"
 #include "specifications.h"
 #include "addressing.h"
 
-static char doc[] = "Encodes data into AX.25 frames";
+static char doc[]      = "Encodes data into AX.25 frames";
 static char args_doc[] = "DESTINATION SOURCE ";
 
 static struct argp_option options[] = {
-	{ "repeater", 'r', "CALLSIGN:SSID", 0,
-		"Repeater station CALLSIGN:SSID", 0 },
-	{ "input",    'i', "FILE",          0, "Input file",  0 },
-	{ "output",   'o', "FILE",          0, "Output file", 0 },
-	{ 0, 0, 0, 0, 0, 0}
+{ "repeater", 'r', "CALLSIGN:SSID", 0, "Repeater station CALLSIGN:SSID", 0 },
+{ "input",    'i', "FILE",          0, "Input file",                     0 },
+{ "output",   'o', "FILE",          0, "Output file",                    0 },
+{ 0,          0,   0,               0, 0,                                0 }
 };
 
 
@@ -81,6 +82,21 @@ static struct argp argp = {
 int main(int argc, char *argv[]) {
 	struct arguments arguments;
 	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	FILE *input = fopen(arguments.input_file, "rb");
+	if (input == NULL) {
+		fprintf(stderr, "Cannot open input file");
+		exit(EX_NOINPUT);
+	}
+	FILE *output = fopen(arguments.output_file, "wb");
+	if (output == NULL) {
+		fprintf(stderr, "Cannot open output file");
+		exit(EX_CANTCREAT);
+	}
+
+
+
+
 
 	return 0;
 }
