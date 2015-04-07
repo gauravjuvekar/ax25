7891c6d38d0ecf8ead6852442f06678135f65e84
Gaurav Juvekar <juvekargs14.it@coep.ac.in>
2015-04-07 20:04:21 +0530

    Rework fcs calc

--- src/fcs.c
+++ src/fcs.c
@@ -1,15 +1,18 @@
 #include "fcs.h"
 #include<stdio.h>
 
-void frame_check(
+bool frame_check(
 		const uint8_t *src, const size_t src_n,
 		uint16_t *dest,
-		struct frame_check_state *state) {
+		struct frame_check_state *state,
+		bool last) {
 	/* Finds the fcs of src_n bytes of src and writes it to destinaton
 	 * state->accumulator must be initialized with an initial value
 	 * state->src_index must be set to 0 before recalling
-	 * if src is changed
 	 * 16 0 bits must be supplied at the end of the data stream
+	 * last must be set to true when the last 16 zero bits are input
+	 *
+	 * returns true if src is comsumed
 	 * 
 	 * polynomial value used is 0x1021
 	 */
@@ -18,43 +21,47 @@ void frame_check(
 	const uint16_t divisor = 0x1021;
 	
 	const unsigned int src_shift_mod         = (8 * sizeof(state->src_mask));
-	const unsigned int accumulator_shift_mod = (8 * sizeof(state->accumulator_mask));
+	const unsigned int accumulator_shift_mod = 
+		(8 * sizeof(state->accumulator_mask));
 	state->src_mask         = 0x1 << (src_shift_mod - 1);
 	state->accumulator_mask = 0x1 << (accumulator_shift_mod - 1);
 
 	while(state->src_index < src_n) {
-		while((state->accumulator & state->accumulator_mask) == 0) {
+		// Shift till msb == 1
+		bool last_iteration; // To shift out that msb
+		for(
+				last_iteration = false;
+				((state->accumulator & state->accumulator_mask) == 0) ||
+					last_iteration; ) {
+			// Shift accumulator
 			state->accumulator <<= 1;
 			if(
 					src[state->src_index] &
 					(state->src_mask >> state->shift_count)) {
 				state->accumulator |= 0x1;
 			}
+			// maintain src masks and pointers
 			state->shift_count += 1;
 			if(!(state->shift_count < src_shift_mod )) {
 				state->shift_count  = 0;
 				state->src_index   += 1;
-				if(!(state->src_index < src_n)) {
-					break;
-				}
+			}
+
+			if(!(state->src_index < src_n)) {
+				break;
+			}
+			if (last_iteration) {
+				break; // preserve last_iteration = true
 			}
 		}
-		// Shift out the MSB 1 since it's going to be zeroed.
-		state->accumulator <<= 1;
-		if(!(state->src_index < src_n)) {
-			break;
+		if(last || last_iteration) {
+			state->accumulator ^= divisor;
 		}
-		if(src[state->src_index] & (state->src_mask >> state->shift_count)) {
-			state->accumulator |= 0x1;
-		}
-		state->shift_count += 1;
-		if(!(state->shift_count < src_shift_mod )) {
-			state->shift_count  = 0;
-			state->src_index   += 1;
-		}
-		state->accumulator ^= divisor;
 	}
+	state->src_index = 0;
 	*dest = state->accumulator;
+	// Src consumed
+	return !last;
 }
 
 
